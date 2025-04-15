dd60710f65d5ee56286eccc2da89754ab1e519e9
Abhijeet Soni <abhisonijeet123@gmail.com>
2025-04-15 12:41:42 +0530

    user controller completed

---
+++ backend/middlewares/authUser.middleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+const authUser = async (req, res, next) => {
+    const { token } = req.cookies || req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+     
+
+        if (decoded.id) {
+            req.body = req.body || {}; // Ensure req.body is defined
+            req.body.userId = decoded.id;
+         
+        } else {
+            return res.status(401).json({ success: false, message: "Unauthorized" });
+        }
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(401).json({ success: false, message: error.message });
+    }
+};
+
+export default authUser;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,33 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import connectDB from './configs/db.js';
+import 'dotenv/config.js';
+import userRouter from './routes/user.route.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+await connectDB();
+
 // Define allowed origins for CORS
 const allowedOrigins = ['http://localhost:5173'];
 
-//middleware configuration
-app.use(express.json());
+// Middleware configuration
+app.use(express.json()); // Add this to parse JSON request bodies
 app.use(cookieParser());
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
 }));
-app.get('/',(req,res)=>{
-    res.send('Hello World!')
-})
 
-app.listen(PORT, ()=>{
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/api/user', userRouter);
+
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
