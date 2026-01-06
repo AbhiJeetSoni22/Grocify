ed9adff7dfcfcd2aa47c74be392b628929d6f6d1
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-01-06 15:20:49 +0530

    token logs aaded

--- backend/middlewares/authUser.middleware.js
+++ backend/middlewares/authUser.middleware.js
@@ -2,13 +2,15 @@ import jwt from "jsonwebtoken";
 
 const authUser = (req, res, next) => {
   const token = req.cookies?.token;
-
+ console.log("All cookies:", req.cookies);
+console.log("Token:", req.cookies?.token);
   if (!token) {
     return res.status(401).json({
       success: false,
       message: "Unauthorized: token missing",
     });
   }
+     
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
